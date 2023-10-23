cee5edce09ba6c8d57005b60d1f25638a2bb22fd
danieldorazio <danieldorazio.97@gmail.com>
2023-10-23 22:51:02 +0200

    increased: JS-MIAL: button click function, record in array

--- js-mail/js/script.js
+++ js-mail/js/script.js
@@ -2,25 +2,56 @@
 const email = ["pippo@gmail.com", "pluto@gmail.com", "paperino@gmail.com", "paperina@gmail.com", "qui@gmail.com", "quo@gmail.com", "qua@gmail.com"];
 
 // INPUT
-const userEmail = prompt("inserisci la tua email");
-console.log(userEmail);
+const userEmail = document.getElementById("mail");
+const userEmailValue = userEmail.value;
+console.log(userEmailValue);
 
+const buttonSend = document.createElement("button");
+buttonSend.innerHTML = "INVIA"
 
-// LOGIC
-let flag = false;
+const inputElem = document.querySelector(".input");
+inputElem.append(buttonSend);
 
-for (let i = 0; i < email.length; i++) {
-    if (userEmail === email[i]) {
-        flag = true;
-        
+// FUNZIONAMEMTO AL CLICK
+
+buttonSend.addEventListener("click", function() {
+    // INPUT
+    const userEmailValue = userEmail.value;
+    console.log(userEmailValue);
+
+    // LOGIC
+    let flag = false;
+
+    for (let i = 0; i < email.length; i++) {
+        if (userEmailValue === email[i]) {
+            flag = true;
+        }
     }
-}
 
-// OUTPUT
+    // OUTPUT
+    const resultUser = document.createElement("div");
+    resultUser.classList.add("resultUser");
+    if (flag) {
+        resultUser.innerHTML = `<p>La tua email è registrata, puoi accedere</p>`
+    } else {
+        resultUser.innerHTML = `<p>Non è registrata, clicca qui se la vuoi registra per registrare</p>`
+        
+        // REGISTRATI BUTTON
+        const addButton = document.createElement("button");
+        addButton.innerHTML = "Registrati";
 
-const resultUser = document.getElementById("result");
-if (flag) {
-    resultUser.innerHTML += `<div>La tua email è registrata, puoi accedere</div>`
-} else {
-    resultUser.innerHTML += `<div>Non è registrata, clicca qui se la vuoi registra per registrare</div>`
-}
+        const resultUserElem = document.querySelector("div");
+        resultUserElem.append(addButton);
+
+        // BUTTON CLICK
+        addButton.addEventListener ("click", function(){
+            email.push(userEmailValue)
+            console.log(email);
+            resultUser.innerHTML = `<p>Registrato</p>`
+        })
+    }
+
+    const containerElem = document.querySelector(".container");
+    containerElem.append(resultUser);
+})
+
