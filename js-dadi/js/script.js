7ccf68ac477583d83beac92408e9db054fbd3e03
danieldorazio <danieldorazio.97@gmail.com>
2023-10-23 19:09:36 +0200

    add: JS-DADI: LOGIC JS

--- js-dadi/js/script.js
+++ js-dadi/js/script.js
@@ -1,1 +1,19 @@
-console.log('aaa');
+const unserNamber = Math.floor(Math.random() * (6 - 1) ) + 1;
+
+const computerNamber = Math.floor(Math.random() * (6 - 1) ) + 1;
+
+let result;
+if (unserNamber > computerNamber) {
+    result = "Sei stato FORTUNATO per questa volta, vediamo se lo sarai ancora"
+} else if (unserNamber === computerNamber){
+    result = "Mi dispiace ma sono usciti due numeri UGUALI, riprova magari sarai più FORTUNATO"
+} else {
+    result = "Mi dispiace, quasta volta sei stato SFORTUNATO, perchè non riprovi, magari la prossima volta sarai più FORTUNATO"
+}
+
+
+const numeriElem = document.getElementById("numeri");
+numeriElem.innerHTML += `Il numero del tuo dado è ${unserNamber} mentre qeullo del tuo avversario è ${computerNamber} ${result}`
+
+const provocationsElem = document.querySelector("p");
+provocationsElem.innerHTML += `${result}`
